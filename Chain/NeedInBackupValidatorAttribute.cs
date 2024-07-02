a4c51cc1bdd80ebefb98855b14f443079d6e0ac5
tim_donin <tim.donin.dev@gmail.com>
2024-07-03 01:07:24 +0300

    Innit commit

---
+++ Chain/NeedInBackupValidatorAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet.Chain
+{
+    [AttributeUsage(
+        AttributeTargets.Class |
+        AttributeTargets.Interface |
+        AttributeTargets.GenericParameter, Inherited = true)]
+    public class NeedInBackupValidatorAttribute(int postErrorRollbackCount) : Attribute
+    {
+        public int PostErrorRollbackCount { get; set; } = postErrorRollbackCount;
+        public int ProcessCounter { get; protected internal set; }
+
+        public bool IsReadyForBackup(bool isDataNull = false)
+        {
+            if (PostErrorRollbackCount <= -1) return isDataNull;
+            return PostErrorRollbackCount % ProcessCounter == 0;
+        }
+    }
+}
---
+++ Chain/VoidHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet.Chain
+{
+    public abstract class VoidHandler: BaseHandler
+    {
+        public VoidHandler(VoidHandler? handler = null)
+        {
+            Next = handler;
+            Type type = this.GetType();
+            ValidatorAttribute =
+                Attribute.GetCustomAttribute(type, typeof(NeedInBackupValidatorAttribute)) as
+                NeedInBackupValidatorAttribute ?? throw new NullReferenceException("Attribute wasn't found");
+        }
+
+    }
+}
